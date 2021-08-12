ede8de67877e4cb008e6248d8a26d0bd23804eaa
hiek2001 <hiek2001@naver.com>
2021-08-12 20:34:13 +0900

    08/12 백준 18406 이코테 p321 문제 풀이

--- CodeTest/src/Test/CodingBook/coding_321.java
+++ CodeTest/src/Test/CodingBook/coding_321.java
@@ -5,12 +5,14 @@ import java.util.*;
 public class coding_321 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		// 1)
 		String num = sc.next();
 		int len = num.length();
 		int[] list = new int[len];
 		for(int i=0 ; i<len ; i++)
 			list[i] = num.charAt(i) - '0';
 		
+		// 2)
 		int mid = len/2;
 		int fsum = 0;
 		int bsum = 0;
