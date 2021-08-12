cce83b3a9da29945b01131188253f73dc72d83f5
hiek2001 <hiek2001@naver.com>
2021-08-12 14:45:37 +0900

    08/12 이코테 그리디 p314 문제 풀이

---
+++ CodeTest/src/Test/CodingBook/coding_314.java
@@ -0,0 +1,22 @@
+package Test.CodingBook;
+
+import java.util.*;
+
+public class coding_314 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0 ; i<n ; i++) 
+			arr[i] = sc.nextInt();
+	
+		Arrays.sort(arr);
+		
+		int target = 1;
+		for(int i=0 ; i<n ; i++) {
+			if(target < arr[i]) break;
+			target += arr[i];
+		}	
+		System.out.println(target);
+	}
+}
