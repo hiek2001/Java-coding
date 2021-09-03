0f16c052b66f1e39cc482ac1e8645ff3229ad5b6
hiek2001 <hiek2001@naver.com>
2021-09-03 13:04:14 +0900

    09/03 백준 별찍기4 문제 풀이

---
+++ CodeTest/src/Test/Baekjoon/bj2441.java
@@ -0,0 +1,19 @@
+package Test.Baekjoon;
+
+import java.util.*;
+
+public class bj2441 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0 ; i<n ; i++) {
+			for(int j=n-i ; j<n ; j++) {
+				System.out.print(" ");
+			}
+			for(int k=i ; k<n ; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
